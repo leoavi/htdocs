c9def02ab63745444700be2689ecaa9964a54dca
Leonardo Avi <leoavivideos@gmail.com>
2021-03-21 22:57:34 -0300

    .
    .

--- model/operacional/ProgramacaoCargaDescargaTabelaModel.php
+++ model/operacional/ProgramacaoCargaDescargaTabelaModel.php
@@ -44,7 +44,7 @@ try {
 
     $name = 2;
 
-    $connect->beginTransaction();
+  
     $sql = "INSERT INTO md_sistema (handle) VALUES (?)";
     $connect->prepare($sql)->execute([$name]);
     $connect->commit();
