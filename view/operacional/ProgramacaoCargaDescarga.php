3d59bedfec5d39deb7731b184ce74afb006b03a1
Leonardo Avi <leoavivideos@gmail.com>
2021-05-21 14:23:58 -0300

    .
    .

--- view/operacional/ProgramacaoCargaDescarga.php
+++ view/operacional/ProgramacaoCargaDescarga.php
@@ -257,9 +257,8 @@ else {
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
             
             <script type="text/javascript" src="../tecnologia/js/ordemvenda/OrdemVendaListarFuncoes.js"></script>
-            <script>
-          
-            <!-- <script type="text/javascript" src="../tecnologia/js/ordemvenda/OrdemVenda.js"></script> -->
+            
+            <script type="text/javascript" src="../tecnologia/js/ordemvenda/OrdemVenda.js"></script>
         </body>
     </html>
 
