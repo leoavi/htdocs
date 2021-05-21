1ab820cf854f6bca263bbf87148c8b297b219938
Leonardo Avi <leoavivideos@gmail.com>
2021-05-21 14:02:58 -0300

    .
    .

--- model/ordemvenda/GravaOrdem.php
+++ model/ordemvenda/GravaOrdem.php
@@ -9,43 +9,10 @@ $connect = Sistema::getConexao();
 $filial = Sistema::getPost('FILIAL');
 $tipo = Sistema::getPost('TIPO');
 $data = Sistema::getPost('DATA');
-$prioridade = Sistema::getPost('PRIORIDADE');
 $cliente = Sistema::getPost('CLIENTE');
-$valortotal = Sistema::getPost('VALORTOTAL');
-$naturezaoperacao = Sistema::getPost('NATUREZAOPERACAO');
-$formapagamento = Sistema::getPost('FORMAPAGAMENTO');
-$condicaopagamento = Sistema::getPost('CONDICAOPAGAMENTO');
-$contatesouraria = Sistema::getPost('CONTATESOURARIA');
-$tabelapreco = Sistema::getPost('TABELAPRECO');
-$vendedor = Sistema::getPost('VENDEDOR');
 $observacao = Sistema::getPost('OBSERVACAO');
-$tipofrete = Sistema::getPost('TIPOFRETE') ? Sistema::getPost('TIPOFRETE') : null;
-$tipotransporte = Sistema::getPost('TIPOTRANSPORTE') ? Sistema::getPost('TIPOTRANSPORTE') : null;
-$transportadora = Sistema::getPost('TRANSPORTADORA') ? Sistema::getPost('TRANSPORTADORA') : null;
-$chave = Sistema::getPost('CHAVE');
 
-WebService::setupCURL("ordemvenda/criar/criarordemvenda", [
-    "FILIAL" => $filial,
-    "TIPO" => $tipo,
-    "DATA" => $data,
-    "VENDEDOR" => $vendedor,
-    "CLIENTE" => $cliente,
-    "TABELAPRECO" => $tabelapreco,
-    "FORMAPAGAMENTO" => $formapagamento,
-    "CONDICAOPAGAMENTO" => $condicaopagamento,
-    "OBSERVACAO" => $observacao,
-    "TIPOFRETE" => $tipofrete,
-    "TIPOTRANSPORTE" => $tipotransporte,
-    "TRANSPORTADORA" => $transportadora,
-    "CHAVE" => $chave,
-    "ORIGEM" => 3
-]);
-
-WebService::execute();
-
-$body = WebService::getBody();
-
-$dados = json_decode($body, true);
+echo $filial
 
 if (isset($dados["CHAVE"])) {
     echo $dados["CHAVE"];
